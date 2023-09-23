f9509354384e5ca665991091dc75a058b18aaea5
Jose Luis Garcia Ruiz <joselugamesofficial@gmail.com>
2023-09-23 13:28:20 +0200

    Improve code order and disable input on moving tiles

--- Assets/Scripts/View/TileView.cs
+++ Assets/Scripts/View/TileView.cs
@@ -29,12 +29,17 @@ namespace Match3.View
         bool isMatchMaster;
         List<TileModel> pendingMatches;
 
+        bool inputActive;
+
         public void Setup(TileModel model, int spawnY)
         {
             this.model = model;
 
             if (spawnY == model.Y)
+            {
                 transform.localPosition = new Vector3(model.X * Size, model.Y * Size);
+                inputActive = true;
+            }
             else
             {
                 transform.localPosition = new Vector3(model.X * Size, spawnY * Size);
@@ -49,28 +54,56 @@ namespace Match3.View
             model.OnFall += OnFall;
         }
 
-        void OnSuccessfulSwap(Direction direction)
+        void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
         {
-            animator.SetInteger(SuccessDirectionParam, (int)direction);
-            animator.SetTrigger(SuccessTrigger);
+            if (!inputActive)
+                return;
+
+            hasMoved = false;
+            dragStart = eventData.position;
         }
 
-        //Called by animator
-        void RefreshPosition()
+        void IDragHandler.OnDrag(PointerEventData eventData)
         {
-            transform.localPosition = new Vector3(model.X * Size, model.Y * Size);
-            gameObject.name = $"Tile {model.X} {model.Y}";
+            if (!inputActive)
+                return;
 
-            model.ResolveMatch();
+            if (hasMoved)
+                return;
+
+            var delta = eventData.position - dragStart;
+            if (delta.magnitude > dragSensitivity)
+            {
+                hasMoved = true;
+                if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+                    TrySwap(delta.x > 0 ? Direction.Right : Direction.Left);
+                else
+                    TrySwap(delta.y > 0 ? Direction.Top : Direction.Down);
+            }
+        }
+
+        void TrySwap(Direction direction)
+        {
+            inputActive = false;
+            model.TrySwap(direction);
         }
 
         void OnFailedSwap(Direction direction)
         {
+            inputActive = true;
             animator.SetTrigger(FailTrigger);
+        }
+
+        void OnSuccessfulSwap(Direction direction)
+        {
+            inputActive = false;
+            animator.SetInteger(SuccessDirectionParam, (int)direction);
+            animator.SetTrigger(SuccessTrigger);
         }
 
         void OnMatch(bool isMasterTile, List<TileModel> matches)
         {
+            inputActive = false;
             isMatchMaster = isMasterTile;
             pendingMatches = matches;
 
@@ -91,6 +124,16 @@ namespace Match3.View
             Destroy(gameObject);
         }
 
+        //Called by animator
+        void RefreshPosition()
+        {
+            transform.localPosition = new Vector3(model.X * Size, model.Y * Size);
+            gameObject.name = $"Tile {model.X} {model.Y}";
+
+            inputActive = true;
+            model.ResolveMatch();
+        }
+
         void OnFall(int oldY, int newY)
         {
             StartCoroutine(FallRoutine(oldY, newY));
@@ -109,33 +152,6 @@ namespace Match3.View
             }
 
             RefreshPosition();
-        }
-
-        void TrySwap(Direction direction)
-        {
-            model.TrySwap(direction);
-        }
-
-        void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
-        {
-            hasMoved = false;
-            dragStart = eventData.position;
-        }
-
-        void IDragHandler.OnDrag(PointerEventData eventData)
-        {
-            if (hasMoved)
-                return;
-
-            var delta = eventData.position - dragStart;
-            if (delta.magnitude > dragSensitivity)
-            {
-                hasMoved = true;
-                if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
-                    TrySwap(delta.x > 0 ? Direction.Right : Direction.Left);
-                else
-                    TrySwap(delta.y > 0 ? Direction.Top : Direction.Down);
-            }
         }
     }
 }
