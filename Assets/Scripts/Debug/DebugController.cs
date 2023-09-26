b2f20e9e338d0913323f0ba3deb6434475feba7d
Jose Luis Garcia Ruiz <joselugamesofficial@gmail.com>
2023-09-26 10:32:18 +0200

    Improve debug options

--- Assets/Scripts/Debug/DebugController.cs
+++ Assets/Scripts/Debug/DebugController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using Match3.Model;
 using Match3.View;
+using PlasticPipe.PlasticProtocol.Messages;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Match3.Debugger
 {
@@ -13,10 +15,24 @@ namespace Match3.Debugger
         [SerializeField] TMP_InputField movesInput;
         [SerializeField] GameView gameView;
 
+        [SerializeField] Button simulateButton;
+        [SerializeField] TMP_Text simulateButtonText;
+        [SerializeField] Button stopButton;
+
+        [SerializeField] Slider timeScaleSlider;
+
+        bool stop = false;
+
         //Called from UI
-        void SimulateMoves()
+        public void SimulateMoves()
         {
             StartCoroutine(SimulateMovesRoutine());
+        }
+
+        //Called from UI
+        public void StopSimulation()
+        {
+            stop = true;
         }
 
         IEnumerator SimulateMovesRoutine()
@@ -27,11 +43,21 @@ namespace Match3.Debugger
                 yield break;
             }
 
+            simulateButton.interactable = false;
+            stopButton.interactable = true;
             var swappableTiles = new List<TileModel>();
-            Time.timeScale = 1;
 
             for (var i = 0; i < moves; i++)
             {
+                if (stop)
+                {
+                    Debug.LogError("Simulation stopped");
+                    break;
+                }
+
+                Time.timeScale = timeScaleSlider.value;
+
+                simulateButtonText.text = $"Simulating {i}/{moves}";
                 swappableTiles.Clear();
                 for (var x = 0; x < gameView.Model.Tiles.GetLength(0); x++)
                 {
@@ -43,6 +69,12 @@ namespace Match3.Debugger
                     }
                 }
 
+                if (swappableTiles.Count == 0)
+                {
+                    Debug.LogError("No valid moves available");
+                    break;
+                }
+
                 var targetTile = swappableTiles[Random.Range(0, swappableTiles.Count)];
                 var direction = targetTile.ValidSwaps.First(kvp => kvp.Value != null).Key;
                 targetTile.TrySwap(direction);
@@ -52,6 +84,11 @@ namespace Match3.Debugger
             }
 
             Time.timeScale = 1;
+
+            simulateButtonText.text = "Simulate";
+            simulateButton.interactable = true;
+            stopButton.interactable = false;
+            stop = false;
         }
     }
 }
